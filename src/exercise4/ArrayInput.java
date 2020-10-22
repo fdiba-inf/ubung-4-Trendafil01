bf8c192b58fe1dabe5cbc6d96695ddeae0fac990
Trendafil01 <trendafil.trendafilov@fdiba.tu-sofia.bg>
2020-10-22 20:45:19 +0000

    Ex4

--- src/exercise4/ArrayInput.java
+++ src/exercise4/ArrayInput.java
@@ -3,11 +3,11 @@ package exercise4;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayInputOutput {
+public class ArrayInput {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int count = scanner.nextInt();
+        int count = input.nextInt();
         int[] numbers = new int[count];
 
         System.out.println("Enter numbers: ");
--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -7,8 +7,9 @@ public class ArrayOutput {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        int[] numbers = new int[5];
+        int count = input.nextInt();
+        int[] numbers = new int[count];
+       
 
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
